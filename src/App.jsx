da811b16a457ad8477cdb654ecccbc9b3107a34a
sarginlar <sarginlar@gmail.com>
2022-02-06 18:46:02 +0300

    son

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,28 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 
-function App() {
-  const [count, setCount] = useState(0)
+// scene ==>sahne
+// geometry ==> şekil
+//material ==> texture,renk
+//mesh ==> geometry,material
 
+const Box = (props) => {
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01));
+  const ref = useRef();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+    <mesh {...props} ref={ref} position={[0, 0, 0]}>
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshBasicMaterial color={"#fff"} />
+    </mesh>
+  );
+};
 
-export default App
+const App = () => {
+  return (
+    <Canvas>
+      <spotLight intensity={1} />
+      <Box />
+    </Canvas>
+  );
+};
+export default App;
